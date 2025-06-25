5158fd3a48089d78b6ed7aa7b6fad10d2b2c41ed
Vinícius Feitosa <viniciusfeitosa@protonmail.com>
2025-06-24 23:47:30 -0300

    Commit 5

--- server/db.ts
+++ server/db.ts
@@ -13,4 +13,4 @@ if (!process.env.DATABASE_URL) {
 }
 
 export const pool = new Pool({ connectionString: process.env.DATABASE_URL });
-export const db = drizzle({ client: pool, schema });
+export const db = drizzle(pool, { schema });
