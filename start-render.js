1465057c63ab1fadf8a214ee847234fc3e13cb21
Vinícius Feitosa <viniciusfeitosa@protonmail.com>
2025-07-25 11:38:03 -0300

    fix: converter start-render.js para ES modules

--- start-render.js
+++ start-render.js
@@ -1,5 +1,9 @@
-const { execSync } = require('child_process');
-const path = require('path');
+import { execSync } from 'child_process';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 console.log('🚀 Iniciando servidor no Render...');
 
